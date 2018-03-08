56208b024f3de0cc8e3a6bfa1c95da95881cd4a2
montgomerysamantha <montgomerysamantha5@gmail.com>
2018-03-08 16:36:11 -0600

    can convert from infix to postfix, initial GUI setup, and classes

---
+++ Project4/UserInterface.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Project4
+{
+    partial class uxUserInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxInitExpLabel = new System.Windows.Forms.Label();
+            this.uxInitTypeLabel = new System.Windows.Forms.Label();
+            this.uxResultTypeLabel = new System.Windows.Forms.Label();
+            this.uxResultLabel = new System.Windows.Forms.Label();
+            this.uxInitialExpressionTextBox = new System.Windows.Forms.TextBox();
+            this.uxInitialTypeComboBox = new System.Windows.Forms.ComboBox();
+            this.uxResultTypeComboBox = new System.Windows.Forms.ComboBox();
+            this.uxResultTextBox = new System.Windows.Forms.TextBox();
+            this.uxConvertExpButton = new System.Windows.Forms.Button();
+            this.uxEvaluateButton = new System.Windows.Forms.Button();
+            this.uxBuildTreeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uxInitExpLabel
+            // 
+            this.uxInitExpLabel.AutoSize = true;
+            this.uxInitExpLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxInitExpLabel.Location = new System.Drawing.Point(13, 9);
+            this.uxInitExpLabel.Name = "uxInitExpLabel";
+            this.uxInitExpLabel.Size = new System.Drawing.Size(133, 18);
+            this.uxInitExpLabel.TabIndex = 0;
+            this.uxInitExpLabel.Text = "Initial Expression: ";
+            // 
+            // uxInitTypeLabel
+            // 
+            this.uxInitTypeLabel.AutoSize = true;
+            this.uxInitTypeLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxInitTypeLabel.Location = new System.Drawing.Point(17, 46);
+            this.uxInitTypeLabel.Name = "uxInitTypeLabel";
+            this.uxInitTypeLabel.Size = new System.Drawing.Size(88, 18);
+            this.uxInitTypeLabel.TabIndex = 1;
+            this.uxInitTypeLabel.Text = "Initial Type: ";
+            // 
+            // uxResultTypeLabel
+            // 
+            this.uxResultTypeLabel.AutoSize = true;
+            this.uxResultTypeLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxResultTypeLabel.Location = new System.Drawing.Point(17, 88);
+            this.uxResultTypeLabel.Name = "uxResultTypeLabel";
+            this.uxResultTypeLabel.Size = new System.Drawing.Size(92, 18);
+            this.uxResultTypeLabel.TabIndex = 2;
+            this.uxResultTypeLabel.Text = "Result type: ";
+            // 
+            // uxResultLabel
+            // 
+            this.uxResultLabel.AutoSize = true;
+            this.uxResultLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxResultLabel.Location = new System.Drawing.Point(12, 149);
+            this.uxResultLabel.Name = "uxResultLabel";
+            this.uxResultLabel.Size = new System.Drawing.Size(162, 18);
+            this.uxResultLabel.TabIndex = 3;
+            this.uxResultLabel.Text = "Resulting Expression: ";
+            // 
+            // uxInitialExpressionTextBox
+            // 
+            this.uxInitialExpressionTextBox.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxInitialExpressionTextBox.Location = new System.Drawing.Point(143, 6);
+            this.uxInitialExpressionTextBox.Name = "uxInitialExpressionTextBox";
+            this.uxInitialExpressionTextBox.Size = new System.Drawing.Size(334, 26);
+            this.uxInitialExpressionTextBox.TabIndex = 4;
+            // 
+            // uxInitialTypeComboBox
+            // 
+            this.uxInitialTypeComboBox.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxInitialTypeComboBox.FormattingEnabled = true;
+            this.uxInitialTypeComboBox.Location = new System.Drawing.Point(116, 43);
+            this.uxInitialTypeComboBox.Name = "uxInitialTypeComboBox";
+            this.uxInitialTypeComboBox.Size = new System.Drawing.Size(121, 26);
+            this.uxInitialTypeComboBox.TabIndex = 5;
+            // 
+            // uxResultTypeComboBox
+            // 
+            this.uxResultTypeComboBox.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxResultTypeComboBox.FormattingEnabled = true;
+            this.uxResultTypeComboBox.Location = new System.Drawing.Point(116, 84);
+            this.uxResultTypeComboBox.Name = "uxResultTypeComboBox";
+            this.uxResultTypeComboBox.Size = new System.Drawing.Size(121, 26);
+            this.uxResultTypeComboBox.TabIndex = 6;
+            // 
+            // uxResultTextBox
+            // 
+            this.uxResultTextBox.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxResultTextBox.Location = new System.Drawing.Point(167, 146);
+            this.uxResultTextBox.Name = "uxResultTextBox";
+            this.uxResultTextBox.Size = new System.Drawing.Size(310, 26);
+            this.uxResultTextBox.TabIndex = 7;
+            // 
+            // uxConvertExpButton
+            // 
+            this.uxConvertExpButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxConvertExpButton.Location = new System.Drawing.Point(12, 208);
+            this.uxConvertExpButton.Name = "uxConvertExpButton";
+            this.uxConvertExpButton.Size = new System.Drawing.Size(169, 33);
+            this.uxConvertExpButton.TabIndex = 8;
+            this.uxConvertExpButton.Text = "Convert Expression";
+            this.uxConvertExpButton.UseVisualStyleBackColor = true;
+            this.uxConvertExpButton.Click += new System.EventHandler(this.ConvertExpressionClick);
+            // 
+            // uxEvaluateButton
+            // 
+            this.uxEvaluateButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxEvaluateButton.Location = new System.Drawing.Point(377, 208);
+            this.uxEvaluateButton.Name = "uxEvaluateButton";
+            this.uxEvaluateButton.Size = new System.Drawing.Size(89, 33);
+            this.uxEvaluateButton.TabIndex = 10;
+            this.uxEvaluateButton.Text = "Evaluate";
+            this.uxEvaluateButton.UseVisualStyleBackColor = true;
+            this.uxEvaluateButton.Click += new System.EventHandler(this.EvaluateClick);
+            // 
+            // uxBuildTreeButton
+            // 
+            this.uxBuildTreeButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxBuildTreeButton.Location = new System.Drawing.Point(225, 208);
+            this.uxBuildTreeButton.Name = "uxBuildTreeButton";
+            this.uxBuildTreeButton.Size = new System.Drawing.Size(109, 33);
+            this.uxBuildTreeButton.TabIndex = 11;
+            this.uxBuildTreeButton.Text = "Build Tree";
+            this.uxBuildTreeButton.UseVisualStyleBackColor = true;
+            this.uxBuildTreeButton.Click += new System.EventHandler(this.BuildTreeClick);
+            // 
+            // uxUserInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 267);
+            this.Controls.Add(this.uxBuildTreeButton);
+            this.Controls.Add(this.uxEvaluateButton);
+            this.Controls.Add(this.uxConvertExpButton);
+            this.Controls.Add(this.uxResultTextBox);
+            this.Controls.Add(this.uxResultTypeComboBox);
+            this.Controls.Add(this.uxInitialTypeComboBox);
+            this.Controls.Add(this.uxInitialExpressionTextBox);
+            this.Controls.Add(this.uxResultLabel);
+            this.Controls.Add(this.uxResultTypeLabel);
+            this.Controls.Add(this.uxInitTypeLabel);
+            this.Controls.Add(this.uxInitExpLabel);
+            this.Name = "uxUserInterface";
+            this.Text = "Expression Tree Builder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label uxInitExpLabel;
+        private System.Windows.Forms.Label uxInitTypeLabel;
+        private System.Windows.Forms.Label uxResultTypeLabel;
+        private System.Windows.Forms.Label uxResultLabel;
+        private System.Windows.Forms.TextBox uxInitialExpressionTextBox;
+        private System.Windows.Forms.ComboBox uxInitialTypeComboBox;
+        private System.Windows.Forms.ComboBox uxResultTypeComboBox;
+        private System.Windows.Forms.TextBox uxResultTextBox;
+        private System.Windows.Forms.Button uxConvertExpButton;
+        private System.Windows.Forms.Button uxEvaluateButton;
+        private System.Windows.Forms.Button uxBuildTreeButton;
+    }
+}
+
---
+++ Project4/UserInterface.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project4
+{
+    public partial class uxUserInterface : Form
+    {
+        public uxUserInterface()
+        {
+            InitializeComponent();
+            List<string> s = new List<string>();
+            s.Add("Infix");
+            s.Add("Prefix");
+            s.Add("Postfix");
+
+            uxInitialTypeComboBox.DataSource = s;
+            uxResultTypeComboBox.BindingContext = new BindingContext();
+            uxResultTypeComboBox.DataSource = s;
+
+            uxInitialTypeComboBox.SelectedItem = 0;
+            uxResultTypeComboBox.SelectedItem = 0;
+        }
+
+        private void ConvertExpressionClick(object sender, EventArgs e)
+        {
+            //infix to postfix conversion
+            if (uxInitialTypeComboBox.Text == "Infix" && uxResultTypeComboBox.Text == "Postfix")
+            {
+                Stack<Char> characters = new Stack<char>();
+                StringBuilder result = new StringBuilder();
+                string init = uxInitialExpressionTextBox.Text;
+
+                for (int i = 0; i < init.Length; i++)
+                {
+                    if (Char.IsNumber(init[i])) //check to see if it's a number
+                    {
+                        //add to result
+                        result.Append(init[i]);
+                    }
+                    else if (Regex.IsMatch(init[i].ToString(), "[+-/*]")) //if it's an operator
+                    {
+                        int cur = OpPreced(init[i]); //finding the operator precedence of the current character
+                        if (characters.Count != 0)
+                        {
+                            int stackop = OpPreced(characters.Peek()); //op preced of stack character
+
+                            while (stackop > cur) //while top stack is an operator whose precedence is greater than that of the current piece
+                            {
+                                result.Append(characters.Pop() + " "); //pop the top element from the stack onto result
+                                if (characters.Count == 0 || characters.Peek() == '(' || characters.Peek() == ')') break;
+                            }
+                        }
+                        characters.Push(init[i]); //push current piece onto stack
+                    }
+                    else if (init[i] == '(')
+                    {
+                        characters.Push(init[i]);
+                    }
+                    else if (init[i] == ')') //If the current piece is a ), 
+                    {
+                        while (characters.Peek() != '(') // pop items from the stack onto result until the top of the stack is a (
+                        {
+                            //pop items from stack onto result 
+                            result.Append(characters.Pop() + " ");
+                        }
+                        characters.Pop(); //to get rid of '('
+                    }
+                    else
+                    {
+                        //whitespace
+                        result.Append(" ");
+                        continue;
+                    }
+                }
+
+                result.Append(" ");
+
+                while (characters.Count != 0) //If items remain on the stack, pop them all onto result
+                {
+                    result.Append(characters.Pop() + " ");
+                }
+                uxResultTextBox.Text = result.ToString();
+            }
+        }
+
+        private int OpPreced(char c)
+        {
+            if ('+' == c || '-' == c)
+            {
+                return 1;
+            }
+            else if ('*' == c || '/' == c)
+            {
+                return 2;
+            }
+            return 0; //not called on an operator
+        }
+
+        private void BuildTreeClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EvaluateClick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
